94d563f02cecd1c8ab034d6bb9bd8fe26e5618d8
Mustafa İlhan <mustafa.ilhan@softtech.com.tr>
2020-09-09 22:49:30 +0300

    Add new videos

--- app/src/data/videos.js
+++ app/src/data/videos.js
@@ -1,5 +1,49 @@
 export default [
   {
+    id: 'sBws8MSXN7A',
+    title: 'React JS Crash Course',
+    subTitle: 'In this crash course you will learn what React JS is and the fundamentals such as components, state, props, JSX, events, etc.',
+    author: '',
+    publisher: 'Traversy Media',
+    language: 'en',
+    duration: '2h',
+    tags: ['React', 'Web'],
+    videoSrc: 'https://www.youtube.com/embed/sBws8MSXN7A'
+  },
+  {
+    id: '-56x56UppqQ',
+    title: 'MongoDB Crash Course',
+    subTitle: 'In this video we will go over MongoDB, which is a popular NoSQL database that is commonly used with Node.js and other technologies.',
+    author: '',
+    publisher: 'Traversy Media',
+    language: 'en',
+    duration: '30min',
+    tags: ['MongoDB', 'Database'],
+    videoSrc: 'https://www.youtube.com/embed/-56x56UppqQ'
+  },
+  {
+    id: 'UB1O30fR-EE',
+    title: 'HTML Crash Course For Absolute Beginners',
+    subTitle: 'In this crash course I will cram as much about HTML that I can. This is meant for absolute beginners. If you are interested in learning HTML but know nothing, then you are in the right place. We will be creating a cheat sheet with all of the common HTML5 tags, attributes, semantic markup, etc.',
+    author: '',
+    publisher: 'Traversy Media',
+    language: 'en',
+    duration: '2h',
+    tags: ['HTML', 'Web'],
+    videoSrc: 'https://www.youtube.com/embed/UB1O30fR-EE'
+  },
+  {
+    id: 'yfoY53QXEnI',
+    title: 'CSS Crash Course For Absolute Beginners',
+    subTitle: 'In this video I will cram as much as possible about CSS. We will be looking at styles, selectors, declarations, etc. We will build a CSS cheat sheet that you can keep as a resource and we will also create a basic website layout. We are using CSS3 but mostly the basics. I will be creating an advanced CSS course with animations,  etc.',
+    author: '',
+    publisher: 'Traversy Media',
+    language: 'en',
+    duration: '2h',
+    tags: ['CSS', 'Web'],
+    videoSrc: 'https://www.youtube.com/embed/yfoY53QXEnI'
+  },
+  {
     id: 'vtPkZShrvXQ',
     title: 'Spring Boot Tutorial for Beginners (Java Framework)',
     subTitle: '',
@@ -15,7 +59,7 @@ export default [
     title: 'Spring Boot - Getting started with Spring Boot (EASY)',
     subTitle: '',
     author: 'Amigoscode',
-    publisher: '',
+    publisher: 'Amigoscode',
     language: 'en',
     duration: '2h',
     tags: ['Spring Boot', 'API'],
